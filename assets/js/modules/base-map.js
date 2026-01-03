b1b41cd3e32ee508528cadfcea6f98322f360731
Fabian Rechsteiner <fabian.rechsteiner@mail.ch>
2026-01-03 12:04:18 +0100

    Add modular MapLibre example page

---
+++ assets/js/modules/base-map.js
@@ -0,0 +1,34 @@
+(() => {
+  const config = window.vectormapExampleConfig || {};
+  const container = config.container || "map";
+
+  if (!window.maplibregl) {
+    console.error("MapLibre GL JS is missing.");
+    return;
+  }
+
+  if (!window.pmtiles) {
+    console.error("PMTiles is missing.");
+    return;
+  }
+
+  if (!maplibregl.__pmtilesProtocolAdded) {
+    const protocol = new pmtiles.Protocol();
+    maplibregl.addProtocol("pmtiles", protocol.tile);
+    maplibregl.__pmtilesProtocolAdded = true;
+  }
+
+  const map = new maplibregl.Map({
+    container,
+    style: config.style || "../styles/ch.vectormap.lightbasemap.json",
+    center: config.center || [8.375, 46.744],
+    zoom: config.zoom ?? 8,
+    bearing: config.bearing ?? 0,
+    pitch: config.pitch ?? 0
+  });
+
+  map.addControl(new maplibregl.NavigationControl(), "top-right");
+
+  window.vectormapModules = window.vectormapModules || {};
+  window.vectormapModules.map = map;
+})();
